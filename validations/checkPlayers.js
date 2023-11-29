76e2ad37d7928876c05c15ff6f30bd65c28ab1f9
James Edmond <jamesedmond@pursuit.org>
2023-11-28 21:13:22 -0500

    Finished checkPlayers in validaions folder

---
+++ validations/checkPlayers.js
@@ -0,0 +1,23 @@
+const checkReviewerName = (req, res, next) => {
+    if (req.body.reviewerName) {
+        console.log("Reviewer name is okay")
+        next()
+    } else {
+        res.status(400).json({ error: "Reviewer Name is required "})
+    }
+};
+
+const checkBoolean = (req, res, next) => {
+
+    if (req.body.favorite === true || req.body.favorite === false) {
+
+        next()
+    } else {
+        res.status(400).json({ error: "favorite must be a boolean value"})
+    }
+};
+
+module.exports = {
+    checkReviewerName,
+    checkBoolean
+}
