79866d265a8febf89333d704d900db4acd09d4bd
James Edmond <jamesedmond@pursuit.org>
2023-12-06 04:55:18 -0500

    Completed

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./app.js");
 require("dotenv").config();
 const PORT = process.env.PORT || 2020;
 app.listen(PORT, () => {
-    console.log(`Bookmarks live on port: ${PORT}`)
+    console.log(`Players live on port: ${PORT}`)
 });
